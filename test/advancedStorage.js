d7557bdc911ab12c48931d0ebd5f17df40957dcb
Jesus6Connex <jesus.velazquez@6connex.com>
2021-09-23 17:28:16 -0500

    refactor(*): use before hook and add variable to handle new element

--- test/advancedStorage.js
+++ test/advancedStorage.js
@@ -1,34 +1,35 @@
 const AdvancedStorage = artifacts.require("AdvancedStorage");
 
+let instance = null;
+before(async () => {
+  instance = await AdvancedStorage.deployed();
+});
+
+const newElement = [10, 20];
+
 contract("AdvancedStorage", () => {
   it("should add elements to the ids array", async () => {
-    const instance = await AdvancedStorage.deployed();
-    const newElement = 10;
-    await instance.add(newElement);
+    await instance.add(newElement[0]);
     const result = await instance.ids(0);
-    assert(newElement === result.toNumber());
+    assert(newElement[0] === result.toNumber());
   });
 
   it("should get elements by position", async () => {
-    const instance = await AdvancedStorage.deployed();
-    const newElement = 20;
-    await instance.add(newElement);
+    await instance.add(newElement[1]);
     const position = 1;
     await instance.get(position);
     const result = await instance.ids(position);
-    assert(newElement === result.toNumber());
+    assert(newElement[1] === result.toNumber());
   });
 
   it("should get all elements from ids array", async () => {
-    const instance = await AdvancedStorage.deployed();
     const rawResult = await instance.getAll();
     const result = rawResult.map((r) => r.toNumber());
-    assert.deepEqual([10, 20], result);
+    assert.deepEqual(newElement, result);
   });
 
   it("should get the length of the ids array", async () => {
-    const instance = await AdvancedStorage.deployed();
     const result = await instance.length();
-    assert(result.toNumber() === 2);
+    assert(result.toNumber() === newElement.length);
   });
 });

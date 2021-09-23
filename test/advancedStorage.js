a117b46e699f13c32f3062a14f26e7e5fa49d129
Jesus6Connex <jesus.velazquez@6connex.com>
2021-09-23 17:17:39 -0500

    add tests for all methods

--- test/advancedStorage.js
+++ test/advancedStorage.js
@@ -6,7 +6,6 @@ contract("AdvancedStorage", () => {
     const newElement = 10;
     await instance.add(newElement);
     const result = await instance.ids(0);
-    console.log(result);
     assert(newElement === result.toNumber());
   });
 
@@ -18,5 +17,18 @@ contract("AdvancedStorage", () => {
     await instance.get(position);
     const result = await instance.ids(position);
     assert(newElement === result.toNumber());
+  });
+
+  it("should get all elements from ids array", async () => {
+    const instance = await AdvancedStorage.deployed();
+    const rawResult = await instance.getAll();
+    const result = rawResult.map((r) => r.toNumber());
+    assert.deepEqual([10, 20], result);
+  });
+
+  it("should get the length of the ids array", async () => {
+    const instance = await AdvancedStorage.deployed();
+    const result = await instance.length();
+    assert(result.toNumber() === 2);
   });
 });

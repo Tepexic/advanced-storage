f3cc3da2abe8e0ce8896b6af4fc7c1374d7c5eeb
Jesus6Connex <jesus@tepexic.com>
2021-09-23 22:32:17 -0500

    connect wallet and trigger methods through it

--- app/src/index.js
+++ app/src/index.js
@@ -1,76 +1,62 @@
 import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import AdvancedStorage from "../../build/contracts/AdvancedStorage.json";
 
-const App = {
-  web3: null,
-  account: null,
-  meta: null,
+let web3 = null;
+let advancedStorage = null;
 
-  start: async function() {
-    const { web3 } = this;
-
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
-        deployedNetwork.address,
-      );
-
-      // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
-
-      this.refreshBalance();
-    } catch (error) {
-      console.error("Could not connect to contract or chain.");
+const initWeb3 = () => {
+  return new Promise(async (resolve, reject) => {
+    // case 1: new metamask is present
+    if (typeof window.ethereum !== undefined) {
+      // ask user permission to connect metamask
+      try {
+        await window.ethereum.enable();
+        resolve(new Web3(window.ethereum));
+      } catch (e) {
+        console.error(e);
+        reject(e);
+      }
     }
-  },
-
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
-
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
-  },
-
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
-  },
-
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
+    // case 2: old metamas is present
+    if (typeof window.web3 !== undefined) {
+      resolve(new Web3(window.web3.currentProvider));
+    }
+    // case3: no metamask - connect to ganache
+    resolve(new Web3("http://localhost:8545"));
+  });
 };
 
-window.App = App;
+const initContract = () => {
+  const deploymentKey = Object.keys(AdvancedStorage.networks)[0];
+  return new web3.eth.Contract(
+    AdvancedStorage.abi,
+    AdvancedStorage.networks[deploymentKey].address
+  );
+};
 
-window.addEventListener("load", function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
-  } else {
-    console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    );
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    );
+const initApp = async () => {
+  const $addData = document.getElementById("add-data");
+  const $data = document.getElementById("data");
+  const accounts = await web3.eth.getAccounts();
+
+  const response = await advancedStorage.methods.getAll().call();
+  $data.innerHTML = response.join(", ");
+
+  $addData.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const data = e.target.elements[0].value;
+    await advancedStorage.methods.add(data).send({ from: accounts[0] });
+    const response = await advancedStorage.methods.getAll().call();
+    $data.innerHTML = response.join(", ");
+  });
+};
+
+window.onload = async function () {
+  try {
+    web3 = await initWeb3();
+    advancedStorage = initContract();
+    initApp();
+  } catch (e) {
+    console.error(e);
   }
-
-  App.start();
-});
+};
